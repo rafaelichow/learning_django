d5e734f594fb9751b5d6942a75c35a62a8146764
Rafael Chow <rafaelichow@gmail.com>
2019-01-01 18:36:03 -0200

    minor changes

---
+++ README.md
@@ -0,0 +1,4 @@
+#learning_django
+
+Created this repository based on the series that can be found at https://www.youtube.com/user/schafer5/videos
+
--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,12 @@
 from django.shortcuts import render
+from ..blog.models import Post
 
-
-posts = [
-    {
-        'author': 'Rafael Chow',
-        'title': 'Blog Post 1',
-        'content': 'First Post',
-        'date_posted': 'Today'
-    },
-    {
-        'author': 'Rafael Chow',
-        'title': 'Blog Post 2',
-        'content': 'Second Post',
-        'date_posted': 'Someday'
-    }
-]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
 def about(request):
-    return render(request, 'blog/about.html', {'title': 'about title'})
-
+    return render(request, 'blog/about.html', {'title': 'About'})
